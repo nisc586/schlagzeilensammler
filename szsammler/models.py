5b9c98e5e0ceeb99de466c6ac5819e18a47e6925
nisc586 <49026680+nisc586@users.noreply.github.com>
2025-02-24 23:25:02 +0100

    Implement a button to fetch articles from an rss feed

--- szsammler/models.py
+++ szsammler/models.py
@@ -1,14 +1,25 @@
 from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import Integer, String, DateTime
 from szsammler import db
+from datetime import datetime
 
 
-class Headline(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str]= mapped_column(nullable=False)
-    link: Mapped[str] = mapped_column()
-    pub_date: Mapped[str] = mapped_column()  # todo: use datetime type for pubDate
-    description: Mapped[str] = mapped_column()
-    channel: Mapped[str] = mapped_column()  # todo: add channel table and foreign key
+class Article(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str]= mapped_column(String, nullable=False)
+    link: Mapped[str] = mapped_column(String, nullable=False)
+    published: Mapped[datetime] = mapped_column(DateTime)
+    description: Mapped[str] = mapped_column(String)
+    # channel: Mapped[str] = mapped_column()  # TODO: add channel table and foreign key
 
     def __repr__(self):
-        return f"Headline(id={self.id}, title={self.title}, pub_date={self.pub_date})"
+        return f"Article(id={self.id}, title={self.title}, pub_date={self.pub_date})"
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "link": self.link,
+            "published": self.published.isoformat(),
+            "description": self.description,
+        }
--- szsammler/routes.py
+++ szsammler/routes.py
@@ -1,10 +1,36 @@
-from flask import Blueprint, render_template
-from .models import Headline
+from flask import Blueprint, render_template, jsonify
+from . import db
+from .models import Article
+import feedparser
+from datetime import datetime
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    count = 0
-    count = Headline.query.count()
+    count = Article.query.count()
     return render_template("index.html", count=count)
+
+@main.route("/fetch-articles", methods=["GET"])
+def fetch_articles():
+    RSS_URL = "https://www.faz.net/rss/aktuell/"  # TODO: Use different newspapers
+    DT_FMT = "%a, %d %b %Y %H:%M:%S %z"
+
+    feed = feedparser.parse(RSS_URL)
+
+    new_articles = []
+    for entry in feed.entries:
+        # Do not insert duplicates into database
+        if not Article.query.filter_by(title=entry.title).first():
+            new_article = Article(
+                title=entry.title,
+                link=entry.link,
+                published=datetime.strptime(entry.published, DT_FMT),
+                description=entry.description,
+                #channel=None  # TODO: Implement newspapers model
+            )
+
+        new_articles.append(new_article.to_dict())
+
+    db.session.commit()
+    return jsonify({"articles" :new_articles})
